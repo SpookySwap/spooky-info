450642809ef168c8fdbba8ed6d8d60ea3e95fc2b
MakiTypeB <MakiTypeB@gmail.com>
2021-04-21 12:49:34 -0700

    renamed ethere to ftm

--- src/components/Warning/index.js
+++ src/components/Warning/index.js
@@ -38,18 +38,18 @@ export default function Warning({ type, show, setShow, address }) {
   const textContent = below800 ? (
     <div>
       <Text fontWeight={500} lineHeight={'145.23%'} mt={'10px'}>
-        Anyone can create and name any ERC20 token on Ethereum, including creating fake versions of existing tokens and
+        Anyone can create and name any ERC20 token on Fantom, including creating fake versions of existing tokens and
         tokens that claim to represent projects that do not have a token.
       </Text>
       <Text fontWeight={500} lineHeight={'145.23%'} mt={'10px'}>
-        Similar to Etherscan, this site automatically tracks analytics for all ERC20 tokens independent of token
+        Similar to FTMscan, this site automatically tracks analytics for all ERC20 tokens independent of token
         integrity. Please do your own research before interacting with any ERC20 token.
       </Text>
     </div>
   ) : (
     <Text fontWeight={500} lineHeight={'145.23%'} mt={'10px'}>
-      Anyone can create and name any ERC20 token on Ethereum, including creating fake versions of existing tokens and
-      tokens that claim to represent projects that do not have a token. Similar to Etherscan, this site automatically
+      Anyone can create and name any ERC20 token on Fantom, including creating fake versions of existing tokens and
+      tokens that claim to represent projects that do not have a token. Similar to FTMscan, this site automatically
       tracks analytics for all ERC20 tokens independent of token integrity. Please do your own research before
       interacting with any ERC20 token.
     </Text>
@@ -75,7 +75,7 @@ export default function Warning({ type, show, setShow, address }) {
                 href={'https://ftmscan.com/address/' + address}
                 target="_blank"
               >
-                View {type === 'token' ? 'token' : 'pair'} contract on Etherscan
+                View {type === 'token' ? 'token' : 'pair'} contract on FTMscan
               </Link>
             </Hover>
             <RowBetween style={{ marginTop: '20px' }}>
