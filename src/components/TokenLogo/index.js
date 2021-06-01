ca33c3c38544f7c310ae3e97030bef1e04be1045
EerieEight <80974420+EerieEight@users.noreply.github.com>
2021-05-31 19:24:50 -0700

    added commented out json fetch

--- src/components/TokenLogo/index.js
+++ src/components/TokenLogo/index.js
@@ -74,6 +74,18 @@ export default function TokenLogo({ address, header = false, size = '24px', ...r
   const path = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${isAddress(
     address
   )}/logo.png`
+  /*
+  let defaultTokenList = ''
+
+  fetch('https://assets.spookyswap.finance/token-list/spookyswap.json',{mode: 'no-cors'})
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (myJson) {
+
+      defaultTokenList = myJson
+      console.log(defaultTokenList)
+    });*/
 
   const localpath = defaultTokenList.tokens.filter((a) => a.address === isAddress(address))
 
