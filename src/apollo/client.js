25a10eff6c0d7c417fe5fac54fe2e05c77b642a4
EerieEight <80974420+EerieEight@users.noreply.github.com>
2021-11-04 10:08:38 -0400

    swap analytics subgraph

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/eerieeight/spooky-swap-exchange'
+    uri: 'https://api.thegraph.com/subgraphs/name/eerieeight/spookyswap'
     ,
   }),
   cache: new InMemoryCache(),
