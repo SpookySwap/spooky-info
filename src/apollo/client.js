2d4ddf618cd7280f596064764e94457ca4dc7da1
EerieEight <80974420+EerieEight@users.noreply.github.com>
2022-03-10 16:26:34 -0500

    Switch to fura

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/eerieeight/spookyswap'
+    uri: 'https://api.fura.org/subgraphs/name/spookyswap'
     ,
   }),
   cache: new InMemoryCache(),
@@ -13,7 +13,7 @@ export const client = new ApolloClient({
 
 export const healthClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/index-node/graphql',
+    uri: 'https://api.fura.org/subgraphs/name/spookyswap',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
@@ -29,7 +29,7 @@ export const stakingClient = new ApolloClient({
 
 export const blockClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/matthewlilley/fantom-blocks',
+    uri: 'https://api.fura.org/subgraphs/name/spookyswap',
   }),
   cache: new InMemoryCache(),
 })
