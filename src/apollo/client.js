c2c66e77b0400c0831fb5868ada9c03cc6092f68
EerieEight <80974420+EerieEight@users.noreply.github.com>
2022-07-22 11:03:42 -0400

    fur afor health

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -13,7 +13,7 @@ export const client = new ApolloClient({
 
 export const healthClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/index-node/graphql',
+    uri: 'https://api.fura.org/subgraphs/name/spookyswap',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
