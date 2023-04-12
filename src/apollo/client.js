628e8a44e616490bbf0abc9358d41f46c1681bf6
0xALUCARD <0xALUCARD@protonmail.com>
2023-04-12 17:46:05 +0200

    replace fura with thegraph

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.fura.org/subgraphs/name/spookyswap'
+    uri: 'https://api.thegraph.com/subgraphs/name/eerieeight/spookyswap'
     ,
   }),
   cache: new InMemoryCache(),
@@ -13,7 +13,7 @@ export const client = new ApolloClient({
 
 export const healthClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.fura.org/subgraphs/name/spookyswap',
+    uri: 'https://api.thegraph.com/subgraphs/name/eerieeight/spookyswap',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
