baea5e37075d687ac1d367575e3ac95bb86a930a
0xALUCARD <0xALUCARD@protonmail.com>
2023-07-11 03:31:47 +0200

    update tokens

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -19,6 +19,34 @@ const TOKEN_OVERRIDES: { [address: string]: { name: string; symbol: string } } =
     name: 'Fantom (Wrapped)',
     symbol: 'FTM',
   },
+  '0x28a92dde19d9989f39a49905d7c9c2fac7799bdf': {
+    name: 'LayerZero USDC',
+    symbol: 'lzUSDC',
+  },
+  '0x695921034f0387eac4e11620ee91b1b15a6a09fe': {
+    name: 'LayerZero WETH',
+    symbol: 'lzWETH',
+  },
+  '0xf1648C50d2863f780c57849D812b4B7686031A3D': {
+    name: 'LayerZero BTC',
+    symbol: 'lzUSDC',
+  },
+  '0x04068da6c83afcfa0e13ba15a6696662335d5b75': {
+    name: 'Multichain (Anyswap) USDC',
+    symbol: 'multiUSDC',
+  },
+  '0x74b23882a30290451a17c44f4f05243b6b58c76d': {
+    name: 'Multichain (Anyswap) WETH',
+    symbol: 'multiWETH',
+  },
+  '0x321162cd933e2be498cd2267a90534a804051b11': {
+    name: 'Multichain (Anyswap) BTC',
+    symbol: 'multiBTC',
+  },
+  '0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e': {
+    name: 'Multichain (Anyswap) DAI',
+    symbol: 'multiDAI',
+  },
 }
 
 // override tokens with incorrect symbol or names
